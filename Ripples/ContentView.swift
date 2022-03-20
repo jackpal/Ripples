24c9bb4af09f453e24868f50bb9036a9fda281f0
jackpal <jack.palevich@gmail.com>
2022-03-20 13:13:48 -0700

    Initial Commit

---
+++ Ripples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ripples
+//
+//  Created by Jack Palevich on 3/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ripples/RipplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RipplesApp.swift
+//  Ripples
+//
+//  Created by Jack Palevich on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RipplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
